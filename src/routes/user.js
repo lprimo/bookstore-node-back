5149777ef671ca00dd7f048fc9fb7a44e175be4f
lprimo <luisfernando@hotmail.com>
2024-07-29 23:18:40 -0300

    uptade tests and gitignore file

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/userController');
 const { authMiddleware, adminMiddleware } = require('../middleware/authMiddleware');
-const { validateUser } = require('../utils/validators');
+const { validateUser } = require('../middleware/validateUserMiddleware');
 
 // Register a new user (protected route, admin only)
 router.post('/register', authMiddleware, adminMiddleware, validateUser, UserController.registerUser);
